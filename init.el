6c6d40619f6fe044e6dc68e34cab2789c21ab3e2
alkc <alkc@proton.me>
2025-03-30 22:23:31 +0200

    hello world

---
+++ init.el
@@ -0,0 +1,117 @@
+;; FOR EARLY-INIT:
+;; (push '(menu-bar-lines . 0) default-frame-alist)
+;; (push '(tool-bar-lines . 0) default-frame-alist)
+;; (push '(vertical-scroll-bars) default-frame-alist)
+
+;; Setup central bkp dir:
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
+;; PKG CONF
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+;; BASIC UI
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)   ; No scrollbar
+(tool-bar-mode -1)     ; No toolbar
+(tooltip-mode -1)      ; No tooltips?
+(set-fringe-mode 10)
+(menu-bar-mode -1)     ; No menu bar
+
+;; Yes to visible bell
+(setq visible-bell t)
+
+;; THEME
+(use-package doom-themes
+  :config
+  (load-theme 'doom-gruvbox t)
+  (doom-themes-visual-bell-config)
+  )
+
+;; UI/LOOKS:
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)   ;; INIT is configured before/right after the pkg is loaded by use-package
+  :custom (doom-modeline-time t) 
+  )
+      
+
+
+;; Line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(dolist
+    (mode '(org-mode-hook
+	    term-mode-hook
+	    shell-mode-hook
+	    eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+;; pkg for logging commands / keys pressed
+(use-package command-log-mode)
+
+;; Ivy autocompletion
+(use-package ivy
+  :diminish ;; Do not show modename in modeline
+  :bind (("C-s" . swiper))
+  :config                    ;; CONFIG is loaded AFTER pkg is loaded/initialized
+  (ivy-mode 1))
+
+(use-package which-key
+  :init (which-key-mode 1)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3)
+)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode)) ;; prog-mode is the base mode of any programming mode
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1)
+  )
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  
+  :config
+  (setq ivy-initial-inputs-alist nil) ;; Don't start searches w/ ^
+  )
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package! nextflow-mode
+  :config
+  (set-docsets! 'nextflow-mode "Groovy"))
